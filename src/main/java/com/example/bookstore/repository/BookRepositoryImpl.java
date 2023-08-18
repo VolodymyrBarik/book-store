3833375c3e0d81ea607ba31d6285d2c4ecb036ff
Volodymyr Barasiuk <barmalej23@gmail.com>
2023-08-18 12:35:08 +0300

    sessions changed to entityManager

--- src/main/java/com/example/bookstore/repository/BookRepositoryImpl.java
+++ src/main/java/com/example/bookstore/repository/BookRepositoryImpl.java
@@ -1,46 +1,39 @@
 package com.example.bookstore.repository;
 
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.EntityTransaction;
 import com.example.bookstore.model.Book;
 import java.util.List;
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
-import org.hibernate.Transaction;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
+@RequiredArgsConstructor
 @Repository
 public class BookRepositoryImpl implements BookRepository {
-    private final SessionFactory sessionFactory;
+    private final EntityManagerFactory entityManagerFactory;
 
-    public BookRepositoryImpl(SessionFactory sessionFactory) {
-        this.sessionFactory = sessionFactory;
-    }
 
     @Override
     public Book save(Book book) {
-        Session session = null;
-        Transaction transaction = null;
-        try {
-            session = sessionFactory.openSession();
-            transaction = session.beginTransaction();
-            session.persist(book);
-            transaction.commit();
+        EntityTransaction entityTransaction = null;
+        try (EntityManager entityManager = entityManagerFactory.createEntityManager()) {
+            entityTransaction = entityManager.getTransaction();
+            entityManager.persist(book);
+            entityTransaction.commit();
             return book;
         } catch (Exception e) {
-            if (transaction != null) {
-                transaction.rollback();
+            if (entityTransaction != null) {
+                entityTransaction.rollback();
             }
             throw new RuntimeException("Can't insert book into db" + book, e);
-        } finally {
-            if (session != null) {
-                session.close();
-            }
         }
     }
 
     @Override
     public List<Book> findAll() {
-        try (Session session = sessionFactory.openSession()) {
-            return session.createQuery("from Book", Book.class).getResultList();
+        try (EntityManager entityManager = entityManagerFactory.createEntityManager()) {
+            return entityManager.createQuery("FROM Book", Book.class).getResultList();
         } catch (Exception e) {
             throw new RuntimeException("Can't get books from db", e);
         }
