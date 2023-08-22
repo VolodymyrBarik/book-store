46f462ab27eb5b95cb229c3f810cd2b8ac5077bd
Volodymyr Barasiuk <barmalej23@gmail.com>
2023-08-22 16:03:40 +0300

    update and delete endpoints added

--- src/main/java/com/example/bookstore/service/BookService.java
+++ src/main/java/com/example/bookstore/service/BookService.java
@@ -10,4 +10,8 @@ public interface BookService {
     List<BookDto> findAll();
 
     BookDto findById(Long id);
+
+    void update(Long id, CreateBookRequestDto dto);
+
+    void delete(Long id);
 }
--- src/main/java/com/example/bookstore/service/BookServiceImpl.java
+++ src/main/java/com/example/bookstore/service/BookServiceImpl.java
@@ -6,7 +6,9 @@ import com.example.bookstore.exception.EntityNotFoundException;
 import com.example.bookstore.mapper.BookMapper;
 import com.example.bookstore.model.Book;
 import com.example.bookstore.repository.BookRepository;
+import java.math.BigDecimal;
 import java.util.List;
+import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -35,5 +37,23 @@ public class BookServiceImpl implements BookService {
         Book book = bookRepository.findById(id)
                 .orElseThrow(() -> new EntityNotFoundException("Can't find book by id " + id));
         return mapper.toDto(book);
+    }
+
+    @Override
+    public void update(Long id, CreateBookRequestDto dto) {
+        Book bookFromDb = bookRepository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException("Can't find book by id " + id));
+        bookFromDb.setAuthor(dto.getAuthor());
+        bookFromDb.setTitle(dto.getTitle());
+        bookFromDb.setDescription(dto.getDescription());
+        bookFromDb.setIsbn(dto.getIsbn());
+        bookFromDb.setPrice(dto.getPrice());
+        bookFromDb.setCoverImage(dto.getCoverImage());
+        bookRepository.save(bookFromDb);
+    }
+
+    @Override
+    public void delete(Long id) {
+        bookRepository.deleteById(id);
     }
 }
