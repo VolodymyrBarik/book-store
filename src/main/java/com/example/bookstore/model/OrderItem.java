ea92ec058fd7250f8554bbd9567d77746bddacc8
Volodymyr Barasiuk <barmalej23@gmail.com>
2023-09-08 17:45:39 +0300

    order and orderItem entities created

---
+++ src/main/java/com/example/bookstore/model/OrderItem.java
@@ -0,0 +1,38 @@
+package com.example.bookstore.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import java.math.BigDecimal;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
+
+@Entity
+@Getter
+@Setter
+@SQLDelete(sql = "UPDATE order_items SET is_deleted=true WHERE id=?")
+@Where(clause = "is_deleted=false")
+@Table(name = "order_items")
+public class OrderItem {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @Column(nullable = false)
+    private Order order;
+    @OneToOne(fetch = FetchType.LAZY)
+    @Column(nullable = false)
+    private Book book;
+    @Column(nullable = false)
+    private int quantity;
+    @Column(nullable = false)
+    private BigDecimal price;
+}
