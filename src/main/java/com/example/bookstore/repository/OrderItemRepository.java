1d47c6d58f1e565b074c1253a2a1b6973f8c40ce
Volodymyr Barasiuk <barmalej23@gmail.com>
2023-09-14 13:02:59 +0300

    Order and orderItem repository added

---
+++ src/main/java/com/example/bookstore/repository/OrderItemRepository.java
@@ -0,0 +1,8 @@
+package com.example.bookstore.repository;
+
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OrderItemRepository extends JpaRepository<OrderItem, Long> {
+    List<OrderItem> findAllByOrderId(Long orderId);
+}
