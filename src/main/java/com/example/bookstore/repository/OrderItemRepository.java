04b3b959be6d63c7f36faf4da64b502cf0155d06
Volodymyr Barasiuk <barmalej23@gmail.com>
2023-09-14 12:38:08 +0300

    initial commit

--- src/main/java/com/example/bookstore/repository/OrderItemRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.bookstore.repository;
-
-import com.example.bookstore.model.OrderItem;
-import java.util.List;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrderItemRepository extends JpaRepository<OrderItem, Long> {
-    List<OrderItem> findAllByOrderId(Long orderId);
-}
